89d02f53a5e99ae37e5170a00daab82a4d387ee1
Megha <meghak1305@gmail.com>
2022-09-19 11:27:06 +0530

    deployed

--- customer/urls.py
+++ customer/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('home',views.customer_home),
+    path('',views.customer_home),
     path('cart',views.customer_cart)
     
 ]
